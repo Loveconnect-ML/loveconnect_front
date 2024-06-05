e4d0dd4be6c26f81c1200cb94fe6292007b99df3
jakepro657 <jakepro657@gmail.com>
2024-06-06 00:08:26 +0900

    feat: new model applied and unnecessary code lines are removed

--- src/app/api/sticker/route.ts
+++ src/app/api/sticker/route.ts
@@ -17,10 +17,6 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const { image } = body;
 
-  // if (password !== process.env.PASSWORD) {
-  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  // }
-
   function b64toBlob(b64Data: string, contentType = "") {
     const image_data = atob(b64Data.split(",")[1]);
 
@@ -46,7 +42,7 @@ export async function POST(request: NextRequest) {
   }
 
   const gptResponse = await openai.chat.completions.create({
-    model: "gpt-4-vision-preview",
+    model: "gpt-4o-2024-05-13",
     messages: [
       {
         role: "system",
@@ -79,42 +75,61 @@ export async function POST(request: NextRequest) {
     ],
   });
 
-  const prompt = gptResponse.choices[0].message.content as string;
+  const gender = gptResponse.choices[0].message.content as string;
 
-  const output = await replicate.run(
-    "zsxkib/instant-id:6af8583c541261472e92155d87bba80d5ad98461665802f2ba196ac099aaedc9",
-    {
-      input: {
-        seed: 32150,
-        image: image,
-        width: 600,
-        height: 800,
-        prompt: `(masterpiece), (detailed), Die-cut ${
-          prompt === "Man" ? "male" : "female"
-        } sticker, Cute kawaii ${
-          prompt === "Man" ? "male" : "female"
-        } character sticker, white background, illustration minimalism, vector, pastel colors`,
-        scheduler: "EulerDiscreteScheduler",
-        enable_lcm: false,
-        sdxl_weights: "protovision-xl-high-fidel",
-        pose_strength: 0.4,
-        canny_strength: 0.3,
-        depth_strength: 0.5,
-        guidance_scale: 5,
-        negative_prompt:
-          "(lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured (lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured, dull, [the: (hands:1.9): 0.5], [the: (fingers:1.9): 0.5], [the: (fingernails:1.9): 0.5]",
-        ip_adapter_scale: 0.8,
-        lcm_guidance_scale: 1.5,
-        num_inference_steps: 30,
-        enable_pose_controlnet: true,
-        enhance_nonface_region: true,
-        enable_canny_controlnet: false,
-        enable_depth_controlnet: false,
-        lcm_num_inference_steps: 5,
-        controlnet_conditioning_scale: 0.8,
+  const gptReponseForDetailed = await openai.chat.completions.create({
+    model: "gpt-4o-2024-05-13",
+    messages: [
+      {
+        role: "system",
+        content: `
+
+        <context>
+          output format is string only with just one words separated by comma.
+
+          example
+          - a person, white blouse, long hair, glasses, smiling
+        </context>
+
+        <instruction>
+          Describe the image in detail.
+          !important: only 5 features of the person in the image.
+        </instruction>
+        `,
       },
-    }
-  );
+      {
+        role: "user",
+        content: [
+          {
+            type: "image_url",
+            image_url: {
+              url: image,
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  const description = gptReponseForDetailed.choices[0].message
+    .content as string;
+
+  const output = await replicate.run(process.env.REPLICATE_MODEL as any, {
+    input: {
+      steps: 20,
+      image: image,
+      width: 600,
+      height: 800,
+      prompt: `(masterpiece), (detailed), ${gender}, ${description}`,
+      upscale_steps: 10,
+      negative_prompt:
+        "ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, low resolution, 3d model, deformed hands, deformed feet, deformed face, deformed body parts, same haircut, eyes without pupils, doubled image, mid aged man, old men, logo in frame, gun, man with more than one penis on body, scared facial expression, drawing, painting, blur focus, blur, photo effects, skinny guy, make-up on male, angry facial expression, same human face in one frame, illustration, anime, cartoon, ugly face, bruises, cartoon, anime, painting, red color saturation, unattractive face, jpeg artifacts, frame, Violence, Gore, Blood, War, Weapons, Death, Destruction, Fire, Explosions, Pollution, Garbage, Graffiti, Vandalism, Rust, Decay, Filth, Disease, Insects, Rodents, Vermin, Darkness, Shadows, Nightmares, Fear, Horror, Sadness, Depression, Pain, Suffering, Anguish, Despair, Loneliness, Isolation, Neglect, Abandonment, Negativity, Hate, Racism, Sexism, Homophobia, Discrimination, Intolerance, Prejudice, Ignorance, Arrogance, Greed, Selfishness, Cruelty, Insanity, Madness, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, deformed, ugly, mutilated, disfigured, text, extra limbs, face cut, head cut, extra fingers, extra arms, poorly drawn face, mutation, bad proportions, cropped head, malformed limbs, mutated hands, fused fingers, long neck, illustration, painting, drawing, art, sketch, disfigured, kitsch, ugly, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, ugly, disgusting, poorly drawn, childish, mutilated, , mangled, old, surreal, decoration, particles",
+      prompt_strength: 4.5,
+      ip_adapter_noise: 0.5,
+      ip_adapter_weight: 0.2,
+      instant_id_strength: 0.7,
+    },
+  });
 
   const imageUrl = (output as string[])?.[0];
 
@@ -127,14 +142,14 @@ export async function POST(request: NextRequest) {
   await prisma?.original.create({
     data: {
       url: originalUrl,
-      gender: prompt,
+      gender: gender,
     },
   });
 
   await prisma?.generated.create({
     data: {
       url: url,
-      gender: prompt,
+      gender: gender,
     },
   });
 
