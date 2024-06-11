cbd7db16dc742e027b82e3711997c6c620954c0a
Jake Yubin KIM <jakepro657@gmail.com>
2024-06-12 07:11:39 +0900

    feat: font and toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
-// import Script from "next/script";
+import localFont from 'next/font/local';
 import {
   ClerkProvider,
 } from '@clerk/nextjs'
+import { Toaster } from 'react-hot-toast';
+import { clsx } from "clsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +20,36 @@ export const viewport: Metadata = {
   viewport: "width=device-width, initial-scale=1",
 };
 
+const pretendardBold = localFont({
+  src: '../../public/Pretendard-Bold.otf',
+  variable: '--font-bold',
+})
+
+const pretendardMedium = localFont({
+  src: '../../public/Pretendard-Medium.otf',
+  variable: '--font-medium',
+})
+
+const pretendardRegular = localFont({
+  src: '../../public/Pretendard-Regular.otf',
+  variable: '--font-regular',
+})
+
+const ibmPlexSansKRSemiBold = localFont({
+  src: '../../public/IBMPlexSansKR-SemiBold.otf',
+  variable: '--font-IBMPlexSansKRSemiBold',
+})
+
+const ibmPlexSansKRMedium = localFont({
+  src: '../../public/IBMPlexSansKR-Medium.otf',
+  variable: '--font-IBMPlexSansKRMedium',
+})
+
+const ibmPlexSansKRBold = localFont({
+  src: '../../public/IBMPlexSansKR-Bold.otf',
+  variable: '--font-IBMPlexSansKRBold',
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,7 +60,8 @@ export default function RootLayout({
       <html lang="en">
         {/*<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9708481151156136"
         crossOrigin="anonymous"></Script>*/}
-        <body className={inter.className}>
+        <body className={clsx(pretendardBold.variable, pretendardMedium.variable, pretendardRegular.variable, inter.className, ibmPlexSansKRSemiBold.variable, ibmPlexSansKRMedium.variable, ibmPlexSansKRBold.variable)}>
+          <Toaster />
           {/* <main className="flex justify-center items-center w-full bg-gray-50 overflow-clip"> */}
           {/* <div className="flex flex-col justify-start items-center w-full sm:w-[500px] overflow-y-scroll scrollbar-hide bg-white font-PretendardBold"> */}
           {/* <SignedOut>
