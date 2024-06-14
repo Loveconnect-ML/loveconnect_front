1a8de90ac1c38b2d0fc7ab80086902636651ed42
Jake Yubin KIM <jakepro657@gmail.com>
2024-06-15 06:42:46 +0900

    feat: adfit added

--- src/app/(Page)/main/page.tsx
+++ src/app/(Page)/main/page.tsx
@@ -3,6 +3,7 @@ import WebcamComponent from "@/components/WebcamComponent";
 import WebcamProvider from "@/components/WebcamProvider";
 import PhotoiskPage from "@/components/pages/PhotoiskPage";
 import React from "react";
+import KakaoAdFit from "@/components/KakaoAdFit";
 
 type Props = {};
 
