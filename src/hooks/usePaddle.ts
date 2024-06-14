edc31f48729f9d378ba847fb28a2da3a043f8891
Jake Yubin KIM <jakepro657@gmail.com>
2024-06-14 23:15:26 +0900

    fix: paddle client token only

--- src/hooks/usePaddle.ts
+++ src/hooks/usePaddle.ts
@@ -9,7 +9,7 @@ export default function usePaddle() {
     initializePaddle({
       environment: process.env.NEXT_PUBLIC_PADDLE_ENV! ? "production" : "sandbox",
       token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
-      seller: process.env.NEXT_PUBLIC_PADDLE_SELLER_ID as unknown as number,
+      // seller: process.env.NEXT_PUBLIC_PADDLE_SELLER_ID as unknown as number,
     } as unknown as InitializePaddleOptions).then((paddleInstance: Paddle | undefined) => {
       if (paddleInstance) {
         setPaddle(paddleInstance);
