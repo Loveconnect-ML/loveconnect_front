ef372951e1379acdb55210df5133929a0a9cc2f9
Jake Yubin KIM <jakepro657@gmail.com>
2024-06-23 14:55:59 +0900

    fix: overflow-x-clip

--- src/app/(Page)/layout.tsx
+++ src/app/(Page)/layout.tsx
@@ -36,7 +36,7 @@ function Layout({ children }: Props) {
 
   return (
     <main className="flex justify-center items-center w-full h-full bg-gray-50 overflow-clip">
-      <div className="select-none flex flex-col justify-start items-center w-full h-full sm:w-[500px] overflow-y-scroll scrollbar-hide font-IBMPlexSansKRSemiBold">
+      <div className="select-none flex flex-col justify-start items-center w-full h-full sm:w-[500px] overflow-y-scroll scrollbar-hide font-IBMPlexSansKRSemiBold overflow-x-clip">
         {children}
         <BottomNavbar>
           {paths.map((path, index) => (
