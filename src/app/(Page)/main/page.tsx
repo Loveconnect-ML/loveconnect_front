e90e3455b5102e3593468c3f2e8c31fe722ae2c6
Jake Yubin KIM <jakepro657@gmail.com>
2024-07-01 08:46:18 +0900

    fix: sw

--- src/app/(Page)/main/page.tsx
+++ src/app/(Page)/main/page.tsx
@@ -92,23 +92,6 @@ function MainPage({}: Props) {
     setDrawer(!drawer);
   };
 
-  useLayoutEffect(() => {
-    addEventListener("fetch", (event: any) => {
-      event.respondWith(
-        (async function () {
-          // Respond from the cache if we can
-          const cachedResponse = await caches.match(event.request);
-          if (cachedResponse) return cachedResponse;
-          // Else, use the preloaded response, if it's there
-          const response = await event.preloadResponse;
-          if (response) return response;
-          // Else try the network.
-          return fetch(event.request);
-        })()
-      );
-    });
-  }, []);
-
   const toggleDiary = async () => {
     toast("일기 작성 기능은 준비중입니다", {
       icon: "🔒",
--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,18 @@
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "serwist";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
@@ -9,4 +21,8 @@ const serwist = new Serwist({
   runtimeCaching: defaultCache,
 });
 
-serwist.addEventListeners();
+serwist.addEventListeners()
+
+// src/serviceWorkerRegistration.js
+
+// Check if service workers are supported
