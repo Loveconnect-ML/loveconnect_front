561d32050e1d09daf0d8a405a2d6bee6667b6b2a
Jake Yubin KIM <jakepro657@gmail.com>
2024-07-13 19:46:21 +0900

    feat: diary api applied

--- src/app/(Page)/main/page.tsx
+++ src/app/(Page)/main/page.tsx
@@ -34,7 +34,20 @@ function MainPage({}: Props) {
     lng: 0,
   });
 
+  const [diary, setDiary] = useState<{
+    title: string;
+    content: string;
+    image: string;
+    result?: string;
+  }>({
+    title: "",
+    content: "",
+    image: "",
+    result: "",
+  });
+
   const [fetching, setFetching] = useState(false);
+  const [loadingGPT, setLoadingGPT] = useState(false);
 
   const { error, location } = useGeo();
 
@@ -101,63 +114,180 @@ function MainPage({}: Props) {
   };
 
   const toggleDiary = async () => {
-    toast("일기 작성 기능은 릴리즈 버전에서 사용 가능합니다", {
-      icon: "🔒",
-    });
+    // toast("일기 작성 기능은 릴리즈 버전에서 사용 가능합니다", {
+    //   icon: "🔒",
+    // });
     setDiaryPage((prev) => !prev);
   };
+
+  const generateDiary = async () => {
+    if (!diary.title || !diary.content || !diary.image) {
+      toast("모든 항목을 입력해주세요", {
+        icon: "🔒",
+      });
+      return;
+    }
+
+    const response = await fetch("/api/v2/ai/diary", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: diary.content,
+        imgUrl: diary.image,
+      }),
+    });
+
+    const { message } = await response.json();
+
+    // console.log(message);
+
+    setDiary((prev) => ({
+      ...prev,
+      result: message,
+    }));
+
+    // setDiary({
+    //   title: diary.title,
+    //   content: message,
+    //   image: diary.image,
+    // });
+
+    toast("일기가 생성되었습니다", {
+      icon: "🔒",
+    });
+  };
+
+  const uploadDiary = async () => {};
 
   return (
     <div className="relative w-full h-full">
       {diaryPage && (
         <div className="absolute flex flex-col items-center left-0 top-0 bg-white w-full h-full z-50">
-          <div className="pt-16 pb-28 relative justify-centerz h-full flex flex-col w-full left-0 overflow-y-auto scrollbar-hide">
-            <div className="z-50 justify-centerz flex flex-col w-full">
-              <div className="text-center w-full text-2xl">
-                AI로 일기와 한줄평 쓰기
-              </div>
-              <div className="font-PretendardRegular text-center w-full text-xs">
-                이미지와 텍스트를 입력하면 AI가 일기와 한줄평을 작성해줍니다
-              </div>
-            </div>
-            <div className="justify-center gap-2 flex-col w-full text-lg top-8 flex items-center px-8 py-6">
-              <div className="w-fit mr-auto">장소</div>
-              <input
-                className="h-10 font-PretendardRegular border-b-2 border-gray-300 text-lg w-full outline-none"
-                placeholder="한줄평 및 일기의 제목을 입력해주세요"
-              />
-            </div>
-            <div className="justify-center gap-2 flex-col w-full text-lg flex items-center px-8 py-6">
-              <div className="w-fit mr-auto">내용 쓰기</div>
-              <textarea
-                className="h-32 font-PretendardRegular border-2 p-3 rounded-lg resize-none border-gray-300 text-lg w-full outline-none"
-                placeholder="내용을 입력해주세요"
-              />
-            </div>
-            <div className="z-50 justify-center gap-2 flex-col w-full text-lg flex items-center px-8 py-6">
-              <div className="w-fit mr-auto">이미지 업로드</div>
-              <div className="h-32 z-50 relative flex flex-col gap-2 items-center justify-center border-2 p-3 rounded-lg resize-none border-gray-300 text-lg w-full outline-none">
-                <input
-                  type={"file"}
-                  accept="image/*"
-                  id="upload-photo"
-                  className="cursor-pointer absolute w-full h-full opacity-0 z-50"
-                />
-                <PlusCircle size={48} className="text-gray-400" />
-                <div className="font-PretendardBold text-gray-400">
-                  이미지를 업로드해주세요
+          {diary.result ? (
+            <div className="z-[70] h-full absolute top-0 justify-center gap-6 flex-col w-full text-lg flex items-center px-8 pt-10 pb-28">
+              <div className=" mr-auto mb-4">
+                <div className="w-fit">생성된 일기</div>
+                <div className="font-PretendardRegular text-xs">
+                  수정하고 업로드할 수 있어요!
                 </div>
               </div>
-            </div>
-            <div className="px-8 pt-4">
-              <Button className=" z-50 justify-center gap-2 flex-col w-full text-lg self-center flex items-center">
-                일기 생성하기
+              <textarea
+                className="h-full font-PretendardRegular border-2 p-3 rounded-lg resize-none border-gray-300 text-lg w-full outline-none"
+                value={diary.result}
+                onChange={(e) =>
+                  setDiary((prev) => ({
+                    ...prev,
+                    result: e.target.value,
+                  }))
+                }
+              />
+              <Button
+                onClick={uploadDiary}
+                className="mt-auto z-50 justify-center gap-2 flex-col w-full self-center flex items-center"
+              >
+                일기 업로드하기
               </Button>
             </div>
-          </div>
+          ) : (
+            <div className="pt-16 pb-28 relative justify-centerz h-full flex flex-col w-full left-0 overflow-y-auto scrollbar-hide">
+              <div className="z-50 justify-centerz flex flex-col w-full">
+                <div className="text-center w-full text-2xl">
+                  AI로 일기와 한줄평 쓰기
+                </div>
+                <div className="font-PretendardRegular text-center w-full text-xs">
+                  이미지와 텍스트를 입력하면 AI가 일기와 한줄평을 작성해줍니다
+                </div>
+              </div>
+              <div className="justify-center flex-col w-full text-lg top-8 flex items-center px-8 py-6">
+                <div className="w-fit mr-auto">제목</div>
+                <input
+                  onChange={(e) =>
+                    setDiary((prev) => ({
+                      ...prev,
+                      title: e.target.value,
+                    }))
+                  }
+                  className="h-10 font-PretendardRegular border-b-2 border-gray-300 text-lg w-full outline-none"
+                  placeholder="한줄평 및 일기의 제목을 입력해주세요"
+                />
+              </div>
+              <div className="justify-center gap-2 flex-col w-full text-lg flex items-center px-8 py-6">
+                <div className="w-fit mr-auto">내용 쓰기</div>
+                <textarea
+                  onChange={(e) =>
+                    setDiary((prev) => ({
+                      ...prev,
+                      content: e.target.value,
+                    }))
+                  }
+                  className="h-32 font-PretendardRegular border-2 p-3 rounded-lg resize-none border-gray-300 text-lg w-full outline-none"
+                  placeholder="내용을 입력해주세요"
+                />
+              </div>
+              <div className="z-50 justify-center gap-2 flex-col w-full text-lg flex items-center px-8 py-6">
+                <div className="w-fit mr-auto">이미지 업로드</div>
+                <div className="h-fit z-50 relative flex flex-col gap-2 items-center justify-center border-2 p-3 rounded-lg resize-none border-gray-300 text-lg w-full outline-none">
+                  <input
+                    onChange={(e) => {
+                      const file = e.target.files?.[0];
+                      if (file) {
+                        const reader = new FileReader();
+                        reader.onload = (e) => {
+                          const result = e.target?.result;
+                          if (typeof result === "string") {
+                            setDiary((prev) => ({
+                              ...prev,
+                              image: result,
+                            }));
+                          }
+                        };
+                        reader.readAsDataURL(file);
+                      }
+                    }}
+                    type={"file"}
+                    accept="image/*"
+                    id="upload-photo"
+                    className="cursor-pointer absolute w-full h-full opacity-0 z-50"
+                  />
+                  {diary.image ? (
+                    <div className="w-32 h-fit">
+                      <Image
+                        src={diary.image}
+                        alt=""
+                        width={128}
+                        height={128}
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      <PlusCircle size={48} className="text-gray-400" />
+                      <div className="font-PretendardBold text-gray-400">
+                        이미지를 업로드해주세요
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+              <div className="px-8 pt-4">
+                <Button
+                  onClick={generateDiary}
+                  className=" z-50 justify-center gap-2 flex-col w-full text-lg self-center flex items-center"
+                >
+                  일기 생성하기
+                </Button>
+              </div>
+              {loadingGPT && (
+                <div className="z-[80] absolute bottom-4 flex flex-col justify-center items-center">
+                  <CircleLoading />
+                </div>
+              )}
+            </div>
+          )}
           <button
             onClick={toggleDiary}
-            className="absolute top-3 right-3 flex items-center justify-center rounded-full bg-white w-10 h-10"
+            className="z-[90] absolute top-3 right-3 flex items-center justify-center rounded-full bg-white w-10 h-10"
           >
             <X fill="black" />
           </button>
@@ -253,7 +383,7 @@ function MainPage({}: Props) {
                       {places?.description}
                     </div>
                   </div>
-                  <div className="font-PretendardRegular text-xs sm:text-sm w-full h-32 overflow-y-scroll scrollbar-hide">
+                  <div className="font-PretendardRegular text-xs sm:text-sm w-[70%] h-32 overflow-y-scroll scrollbar-hide">
                     {places?.overview}
                   </div>
                 </div>
